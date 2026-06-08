1610b3ebb828961c1e98b3d99886426d2d932419
fabioadelfio <adelfiofabio07@gmail.com>
2026-06-08 16:58:02 +0200

    fix SPA netlify deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,6 @@ export const routes: Routes = [
     loadComponent: () => import('./features/home/home.component').then((m) => m.HomeComponent),
   },
   {
-    path: 'about',
-    loadComponent: () => import('./features/about/about.component').then((m) => m.AboutComponent),
-  },
-  {
     path: 'projects',
     loadComponent: () =>
       import('./features/projects/projects.component').then((m) => m.ProjectsComponent),
@@ -20,10 +16,5 @@ export const routes: Routes = [
       import('./features/projects/project-detail/project-detail.component').then(
         (m) => m.ProjectDetailComponent,
       ),
-  },
-  {
-    path: 'contact',
-    loadComponent: () =>
-      import('./features/contact/contact.component').then((m) => m.ContactComponent),
   },
 ];
